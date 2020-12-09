223c9620c640663f3c4fb534b3fed93e13e5de56
Luca Modica <luca.modica55@gmail.com>
2020-12-09 01:32:12 +0100

    Other tests

--- hello/Program.cs
+++ hello/Program.cs
@@ -7,6 +7,8 @@ namespace hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
---
+++ testObj/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace testObj{
+
+    class Program{
+
+        static void Main(string[] args){
+            Car car = new Car(5,"red");
+            car.Color = "red";
+            car.Price = 4.5;
+            Console.WriteLine(car.Price);
+        }
+    }
+}
